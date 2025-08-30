a7ce4d185da473a89334108c02a2e4cc8724a60c
jianh-ai <70783403+jianh-ai@users.noreply.github.com>
2025-08-30 17:20:05 +0800

    Create notability.js

---
+++ script/notability.js
@@ -0,0 +1,29 @@
+// 2024-02-08
+
+var obj = JSON.parse($response.body);
+obj = {
+   "data" : {
+     "processAppleReceipt" : {
+       "error" : 0,
+       "subscription" : {
+         "productId" : "com.gingerlabs.Notability.premium_subscription",
+         "originalTransactionId" : "570001184068302",
+         "tier" : "premium",
+         "refundedDate" : null,
+         "refundedReason" : null,
+         "isInBillingRetryPeriod" : false,
+         "expirationDate" : "2099-09-09T09:09:09.000Z",
+         "gracePeriodExpiresAt" : null,
+         "overDeviceLimit" : false,
+         "expirationIntent" : null,
+         "__typename" : "AppStoreSubscription",
+         "user" : null,
+         "status" : "canceled",
+         "originalPurchaseDate" : "2022-09-09T09:09:09.000Z"
+       },
+       "__typename" : "SubscriptionResult"
+    }
+  }
+};
+
+$done({body: JSON.stringify(obj)});
